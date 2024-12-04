9f1638e39060ee28c7f0a14862ac6f918a85d93b
Jack Underhill <100008285+Jack-Underhill@users.noreply.github.com>
2024-12-03 17:09:22 -0800

    Moved User Verification into User.cs

--- Class_Scheduler_322/Scheduler/User.cs
+++ Class_Scheduler_322/Scheduler/User.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace Scheduler
 {
-    internal class User
+    public class User
     {
+        private string email;
+    
+        public User(string email)
+        {
+            this.email = email;
+        }
+    
+        public string isValidated(MySqlConnection con)
+        {
+            string validateQuery = "SELECT COUNT(*) FROM wsu_users WHERE e_mail = @Email";
+    
+            try
+            {
+                using (con)
+                {
+                    con.Open();
+                    using (MySqlCommand cmd = new MySqlCommand(validateQuery, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Email", email);
+    
+                        int count = Convert.ToInt32(cmd.ExecuteScalar());
+                        if(count > 0)
+                            return "True";
+                        else
+                            return "False";
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                return ex.Message;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
     }
 }
